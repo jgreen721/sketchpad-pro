f2a4dc6ee031aea28ca22b5e073dd91c06d87b50
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-09-25 13:00:35 -0700

    save

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter as Router} from "react-router-dom"
 import './index.css'
-import { AppProvider } from './context/appcontext.jsx'
+import { AppProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
